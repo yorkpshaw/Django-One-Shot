bcea3e86b39d0fd78a6f28532907be43cacfba29
Bart Dorsey <bart@bartdorsey.com>
2022-06-14 07:13:34 -0500

    Adding admin files

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from todos.models import TodoItem, TodoList
 # Register your models here.
+
+class TodoItemAdmin(admin.ModelAdmin):
+    pass
+
+class TodoListAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TodoList, TodoListAdmin)
+admin.site.register(TodoItem, TodoItemAdmin)
